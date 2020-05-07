1adf68db8cbad859c9ef12f0b387bec7977eaf8a
Florin Vasiliu <favasiliu@gmail.com>
2020-05-07 11:14:13 -0500

    added function to populate html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+// populating table function
+function populateTable(dataToDisplay){
+    // iterating through each row of data and adding row to table
+    dataToDisplay.forEach(rowData => {
+        var tblBody = d3.select("#ufo-table>tbody");
+        tblRow = tblBody.append("tr");
+        //iterating through each property of data and adding value to cell
+        Object.entries(rowData).forEach(cellData =>{
+            tblCell = tblRow.append("td")
+            tblCell.text(cellData[1])
+        })
+    });
+}
+
+
+populateTable(data);
